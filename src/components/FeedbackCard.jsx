177f90f840be01f5a593801719bc5fc7cf79b8cb
Achmad Furqon Rachmadie <knowledge.oz2704@gmail.com>
2023-01-05 11:46:14 +0700

    perbaiki property yang salah

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,4 +1,5 @@
 import { quotes } from '../assets'
+
 const FeedbackCard = ({ content, name, title, img }) => (
   <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
     <img src={quotes} alt="double_quotes" className="w-[42px] h-[27px] object-contain" />
@@ -9,8 +10,7 @@ const FeedbackCard = ({ content, name, title, img }) => (
 
       <div className="flex flex-col ml-4">
         <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-white">{name}</h4>
-        <p className="font-poppins font-semibold text-[16px] leading-[24px] text-dimWhite">{name}</p>
-        
+        <p className="font-poppins font-semibold text-[16px] leading-[24px] text-dimWhite">{title}</p>
       </div>
     </div>
   </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,14 +11,12 @@ const Navbar = () => {
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
 
-      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+      <ul className="list-none sm:flex hidden flex-1 justify-end items-center ">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
             className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}>
-            <a href={`#${nav.id}`}>
-              {nav.title}
-            </a>
+            <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
         ))}
       </ul>
